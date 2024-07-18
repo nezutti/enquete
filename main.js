9206b5bdc1c8a2ef9e8b5c621a11f07993843b65
久保祐輔 <bokukuboyu@icloud.com>
2024-07-18 23:18:26 +0900

    changejava

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const button = document.getElementById("loadButton");
 
 
 button.addEventListener("click", function () {
+  button.innerHTML = `<div class="load"></div>`;
+  
   setTimeout(function () {
+    
     location.replace("submit.html")
   }, 4000);
 });
